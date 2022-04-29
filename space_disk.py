b955de3814a659162851f6f5023bbfd32eb8d4f6
Владимир Соломаха <solomakha_vladimir@smart-algoritm.ru>
2022-04-29 15:34:55 +0300

    release 1

---
+++ space_disk.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import configparser
+import psutil
+import requests
+import schedule
+import time
+
+HOSt = 'http://85.175.98.17:1024/Shops/hs/Tasks/ExchangeTasks'
+GUID = 'f7a9a358-f00f-41b0-8618-c42ecd1e8c42'
+
+
+def check_disk(disk, size):
+    free = psutil.disk_usage(disk).free/(1024*1024*1024)
+    message: str = f"{free:.4} Gb free on disk {disk}"
+    #print(message)
+    if(free > size):
+        return
+    data = {'GUID': GUID,
+            'MessageTelegram': message.encode('utf-8')}
+    try:
+        requests.post(f"http://{HOSt}/", data)
+    except:
+        print(f'error send post request on disk {disk}')
+
+
+def main():
+    cf = configparser.ConfigParser()
+    cf.read('disks')
+    count_disks = int(cf.get('options', 'count_disks'))
+    for nom in range(1, count_disks+1):
+        path = cf.get('disk'+str(nom), 'path')
+        size = int(cf.get('disk'+str(nom), 'size'))
+        check_disk(path, size)
+
+
+if __name__ == '__main__':
+    cf = configparser.ConfigParser()
+    cf.read('disks')
+    count_time = int(cf.get('options', 'count_times'))
+    for nom in range(1, count_time+1):
+        new_time = cf.get('time', 'time'+str(nom))
+        schedule.every().day.at(new_time).do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
